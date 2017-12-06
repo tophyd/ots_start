d3433f9000ccc83f2f4b969b37b231fdfc8beb14
stv <yarakaeff@gmail.com>
2017-12-06 16:02:26 +0300

    add lib.h test.cpp

---
+++ lib.h
@@ -0,0 +1,10 @@
+//
+// Created by sv.yarakayev on 06.12.2017.
+//
+
+#ifndef OTUS01_LIB_H
+#define OTUS01_LIB_H
+
+int version();
+
+#endif //OTUS01_LIB_H_H
---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sv.yarakayev on 06.12.2017.
+//
+
+#include "test.h"
